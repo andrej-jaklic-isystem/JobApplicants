c6b4a6db9656fd22250f19ba2db59cc77553386d
iSystemLabs01 <57613987+iSystemLabs01@users.noreply.github.com>
2019-11-11 08:14:45 +0100

    Test01 skeleton
    Simple web application, based on bootstrap. No database context

---
+++ Test01/Test01/Models/Category.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test01.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            InverseIdParentNavigation = new HashSet<Category>();
+            TopicCategory = new HashSet<TopicCategory>();
+        }
+
+        public int Id { get; set; }
+        public int? IdParent { get; set; }
+        public int? IdTemplate { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Imageurl { get; set; }
+        public string Cuser { get; set; }
+        public DateTime? Cdate { get; set; }
+        public string Muser { get; set; }
+        public DateTime? Mdate { get; set; }
+
+        public virtual Category IdParentNavigation { get; set; }
+        public virtual ICollection<Category> InverseIdParentNavigation { get; set; }
+        public virtual ICollection<TopicCategory> TopicCategory { get; set; }
+     
+    }
+}
---
+++ Test01/Test01/Models/TopicFeedback.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test01.Models
+{
+    public partial class TopicFeedback
+    {
+        public int Id { get; set; }
+        public int TopicId { get; set; }
+        public int FeedbackTypeId { get; set; }
+        public DateTime FeedbackDate { get; set; }
+        public string FeedbackComment { get; set; }
+        public string Cuser { get; set; }
+        public DateTime Cdate { get; set; }
+        public string Muser { get; set; }
+        public DateTime? Mdate { get; set; }
+
+        public virtual Topic Topic { get; set; }
+    }
+}
